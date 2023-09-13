983fdf671f01e31b3affa237cb768320a30b7ec2
Marco Fabian <marcofabianufmg@hotmail.com>
2023-09-13 10:09:35 -0300

    alterações no encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,15 +3,15 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+
 def test_encrypt_message():
-    with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message("jaxmuitoop", "2")
-
-    with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message(123, 2)
-
-    assert encrypt_message("jaxmuitoop", 10) == "axmuitoopj"
-
-    assert encrypt_message("jaxmuitoop", 3) == "xaj_muipoot"
-
-    assert encrypt_message("jaxmuitoop", 4) == "mutoop_axji"
+    with pytest.raises(TypeError):
+        encrypt_message(1, "key")
+    with pytest.raises(TypeError):
+        encrypt_message('humm', 'humm')
+    assert encrypt_message("hello", 6) == "olleh"
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("hello", 4) == "o_lleh"
